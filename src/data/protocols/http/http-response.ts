5f5435500c1a08335fbf0020a2a92bb07fcf9e6f
Lucas Barros <lbarros.comp@gmail.com>
2021-11-03 04:53:32 -0300

    refactor: adding generics

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -8,7 +8,7 @@ export enum HttpStatusCode {
   internalServerError = 500,
 }
 
-export type HttpResponse = {
+export type HttpResponse<T> = {
   statusCode: HttpStatusCode
-  body?: any
+  body?: T
 }
