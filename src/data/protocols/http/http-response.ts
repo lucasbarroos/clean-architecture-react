6889d7227bae5780bafb162ab846711d17e93ce2
Lucas Barros <lbarros.comp@gmail.com>
2021-11-03 04:43:08 -0300

    test: inserting the success and error tests

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,6 +1,10 @@
 export enum HttpStatusCode {
+  ok = 200,
   noContent = 204,
-  unauthorized = 401
+  notModified = 304,
+  badRequest = 400,
+  unauthorized = 401,
+  internalServerError = 500,
 }
 
 export type HttpResponse = {
