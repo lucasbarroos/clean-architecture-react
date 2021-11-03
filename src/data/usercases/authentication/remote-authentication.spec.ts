17a1dbe5f1dcc4b24f3a58b26eb1502c4d7274aa
Lucas Barros <lbarros.comp@gmail.com>
2021-11-03 03:21:27 -0300

    refactor: move remote-authentication interfaces to another folder

--- src/data/usercases/authentication/remote-authentication.spec.ts
+++ src/data/usercases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,8 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { HttpClientSpy } from '../..//test/mock-http-client'
 
 describe('RemoteAuthenticaton', () => {
   test('Should call HttpClient with correct URL', async () => {
-    class HttpClientSpy implements HttpPostClient {
-      url?: string
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'https://www.google.com'
     const httpPostClientSpy = new HttpClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
